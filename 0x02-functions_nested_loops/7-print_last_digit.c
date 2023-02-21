cfdae47bb058d863bfa37e53727e9f22f5ccaa12
Samojah <smjfatma898@gmail.com>
2023-02-21 12:34:52 -0800

    ./7-last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * print_last_digit -Prints the last digit of a number
+ * print_last_digit - Prints the last digit of a number
  * @n: The number to be treated
  * Return: Value of the last digit of number
  */
@@ -14,6 +15,6 @@ int print_last_digit(int n)
 	{
 	last = last * -1;
 	}
-	_putchar(last + 0);
+	_putchar(last + '0');
 	return (last);
 }
