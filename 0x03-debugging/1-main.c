24ddf2022dfea630906ce16207f2ea0561d74e98
Samojah <smjfatma898@gmail.com>
2023-02-23 09:49:22 -0800

    main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,6 @@
 int main(void)
 {
 	 int i;
-
 	 printf("Infinite loop incoming :(\n");
 
 	 i = 0;
@@ -19,6 +18,5 @@ int main(void)
 	 /*}*/
 
 	 printf("Infinite loop avoided! \\o/\n");
-
 	 return (0);
 }
