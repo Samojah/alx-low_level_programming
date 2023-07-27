4028c432a6fcc0a2976bdd6c10efc36f443909f9
Samojah <smjfatma898@gmail.com>
2023-07-27 13:07:33 -0700

    linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -15,8 +15,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *temp = *head;
 	unsigned int len = 0;
 	while (str[len])
+
 	len++;
- 	new = malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
 	if (!new)
 	return (NULL);
 	new->str = strdup(str);
